8be5714e451151ed9d061f47d1dfab80a1b2a793
jhons14 <tivenorjuela7@gmail.com>
2024-03-06 06:34:54 -0500

    Adaptando sistema de mesa con cambio en el product menu, siguiente adaptar el router

--- src/Pages/OrderSection/index.jsx
+++ src/Pages/OrderSection/index.jsx
@@ -1,17 +1,70 @@
-function OrderSection(props) {
-  const renderView = () =>
-    props.orderList[props.tableActive - 1]?.products.map((product) => (
-      <li key={product.id}>{product}</li>
-    ));
+import { useRef, useState } from 'react';
+
+function OrderSection({ tableActive, setTableActive, orderList }) {
+  console.log(orderList);
+  const formRef = useRef();
+  const [openCreateTableBill, setOpenCreateBill] = useState(false);
+  const [mesas, setMesas] = useState([
+    {
+      id: '1',
+      nombreCliente: 'Jhon',
+    },
+    {
+      id: '2',
+      nombreCliente: 'Pedro',
+    },
+    {
+      id: '3',
+      nombreCliente: '',
+    },
+    {
+      id: '4',
+      nombreCliente: '',
+    },
+  ]);
+  const checkInTable = () => {
+    const formData = new FormData(formRef.current);
+    console.log(tableActive);
+    mesas[tableActive.id - 1].nombreCliente = formData.get('nombre-cliente');
+    setOpenCreateBill(false);
+  };
+  const createBill = () => {
+    return (
+      <form ref={formRef}>
+        <p>Nombre del cliente</p>
+        <input type='text' name='nombre-cliente' id='nombre-cliente' />
+        <button type='button' onClick={() => checkInTable()}>
+          Registrar
+        </button>
+      </form>
+    );
+  };
   return (
-    <div className='Table-section'>
-      <section>
-        <button onClick={() => props.setTableActive(1)}>Table 1</button>
-        <button onClick={() => props.setTableActive(2)}>Table 2</button>
-      </section>
-      <section>
-        <ul>{renderView()}</ul>
-      </section>
+    <div className='bill-section-container'>
+      {!!tableActive && (
+        <section>
+          <p className='bill-section-title'>{`Mesa ${tableActive} - ${
+            mesas.find((element) => tableActive === element.id).nombreCliente
+          }`}</p>
+          {!tableActive.nombreCliente && (
+            <button onClick={() => setOpenCreateBill(true)}>
+              Registrar cuenta en mesa
+            </button>
+          )}
+        </section>
+      )}
+      <section>{!!openCreateTableBill && createBill()}</section>
+      <div>
+        <ul>
+          <li>
+            {mesas.map((mesa) => (
+              <button key={mesa.id} onClick={() => setTableActive(mesa.id)}>
+                Mesa {mesa.id}
+              </button>
+            ))}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
