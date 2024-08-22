557531008f506e78efb3dfe1f9f1042282d33260
Jhons14 <tivenorjuela7@gmail.com>
2024-08-22 13:31:27 -0500

    Some fixes

--- src/Components/ProductDetails/index.jsx
+++ src/Components/ProductDetails/index.jsx
@@ -12,15 +12,15 @@ function ProductDetails({
   typeProductActive,
 }) {
   const AUTHENTICATION_URL =
-    'http://https://server-market-production.up.railway.app/platzi-market/api/auth/authenticate';
+    'https://server-market-production.up.railway.app/platzi-market/api/auth/authenticate';
 
   const UPLOAD_PRODUCTIMG_URL =
-    'http://https://server-market-production.up.railway.app/platzi-market/api/files/upload/image/product/';
+    'https://server-market-production.up.railway.app/platzi-market/api/files/upload/image/product/';
 
   const UPDATE_PRODUCT_URL =
-    'http://https://server-market-production.up.railway.app/platzi-market/api/products/update/';
+    'https://server-market-production.up.railway.app/platzi-market/api/products/update/';
 
-  const IMG_URL = `http://https://server-market-production.up.railway.app/platzi-market/api/images/products/${product.img_url}`;
+  const IMG_URL = `https://server-market-production.up.railway.app/platzi-market/api/images/products/${product.img_url}`;
 
   const navigate = useNavigate();
   const sizesToShow = ['Small', 'Medium', 'Largue'];
